a8115d1b1744b00028df6b9aa14f06005a98304b
shun7603 <summersao7603@icloud.com>
2025-04-17 15:03:04 +0900

    修正：ユーザー管理機能
    last_name,first_nameのバリデーション設定
    email,last_name_first_name,passwordの単体テストコード設定

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,6 +27,14 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Email is invalid')
       end
+      it '重複するemailでは登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
+      end
+
       it 'passwordが空では登録できない' do
         @user.password = ''
         @user.valid?
@@ -50,15 +58,38 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Password は半角英数字混合で入力してください')
       end
+      it 'passwordに全角文字を含んでいると登録できない' do
+        @user.password = '12wｑｑｑ'
+        @user.password_confirmation = '12wｑｑｑ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password は半角英数字混合で入力してください')
+      end
+      it 'passwordが6文字以下では登録できない' do
+        @user.password = '123aa'
+        @user.password_confirmation = '123aa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password は6文字以上で入力してください')
+      end
+
       it 'last_nameが空では登録できない' do
         @user.last_name = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Last name can't be blank")
       end
+      it 'last_nameにひらがな・カタカナ・漢字以外の入力では登録できない' do
+        @user.last_name = 'やまだa/1'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Last name はひらがな・カタカナ・漢字で入力してください')
+      end
       it 'first_nameが空では登録できない' do
         @user.first_name = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("First name can't be blank")
+      end
+      it 'first_nameにひらがな・カタカナ・漢字以外の入力では登録できない' do
+        @user.first_name = 'しゅんa/2'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('First name はひらがな・カタカナ・漢字で入力してください')
       end
       it 'last_name_kanaが空では登録できない' do
         @user.last_name_kana = ''
