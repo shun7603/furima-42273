8aaf075c89252fe26646510368de87dc1831e469
shun7603 <summersao7603@icloud.com>
2025-04-21 15:38:15 +0900

    商品情報編集機能
    itemコントローラーにedit,update,show実装
    edit.html編集
    show.html編集
    index.htmlにてshow.htmlに遷移できるように編集
    ルーティング設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:edit, :update, :show]
+  before_action :move_to_index, only: [:edit, :update]
   def index
     @items = Item.order('created_at DESC')
   end
@@ -18,7 +19,34 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @item = Item.find(params[:id])
+
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
   private
+
+  def move_to_index
+    return redirect_to root_path unless user_signed_in?
+    return unless current_user != @item.user || @item.order.present?
+
+    redirect_to root_path
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def item_params
     params.require(:item).permit(:item_name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id,
